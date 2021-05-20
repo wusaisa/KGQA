83e9ec59a2765e39fb599a66841b1b496dfe584c
张伟 <jtyoui@qq.com>
2021-05-20 10:25:38 +0800

    优化

--- relation/service/model.py
+++ relation/service/model.py
@@ -2,13 +2,38 @@ import torch
 import torch.nn as nn
 from pytorch_pretrained_bert import BertModel, BertTokenizer
 
+key = {0: '别名',
+       1: '分布区域',
+       2: '病原学名',
+       3: '病原中文名',
+       4: '病原属性',
+       5: '为害部位',
+       6: '为害作物',
+       7: '属目',
+       8: '属科',
+       9: '学名',
+       10: '防治药物',
+       11: '症状',
+       12: '病原',
+       13: '传播途径和发病条件',
+       14: '病因',
+       15: '防治方法',
+       16: '为害特点',
+       17: '形态特征',
+       18: '生活习性',
+       19: '生态特点',
+       20: '生态史',
+       21: '简介'
+       }
+
 
 class Config:
     """配置参数"""
 
     def __init__(self):
-        self.class_list = [str(i) for i in range(11)]  # 类别名单
-        self.save_path = '/app/model/ERNIE'
+        self.dir = '/app/model/'
+        self.class_list = [str(i) for i in range(len(key))]  # 类别名单
+        self.save_path = self.dir + 'ERNIE'
         self.device = torch.device('cpu')
         self.require_improvement = 1000  # 若超过1000batch效果还没提升，则提前结束训练
         self.num_classes = len(self.class_list)  # 类别数
@@ -16,7 +41,7 @@ class Config:
         self.batch_size = 128  # mini-batch大小
         self.pad_size = 32  # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-5  # 学习率
-        self.bert_path = '/app/model/bert'
+        self.bert_path = self.dir + 'bert'
         self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
         self.hidden_size = 768
 
@@ -51,18 +76,6 @@ class Model(nn.Module):
 config = Config()
 model = Model(config).to(config.device)
 model.load_state_dict(torch.load(config.save_path, map_location='cpu'))
-key = {0: '别名',
-       1: '分布区域',
-       2: '病原学名',
-       3: '病原中文名',
-       4: '病原属性',
-       5: '为害部位',
-       6: '为害作物',
-       7: '属目',
-       8: '属科',
-       9: '学名',
-       10: '防治农药'
-       }
 
 
 def prediction_model(text):
@@ -75,4 +88,4 @@ def prediction_model(text):
 
 
 if __name__ == '__main__':
-    print(prediction_model('水稻恶苗病主要危害哪些部位？'))
+    print(prediction_model('大麻跳甲的学名？？'))
