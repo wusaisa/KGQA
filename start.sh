080d9ac92a9e5a83f1d6cf125ec78b168702565d
张伟 <jtyoui@qq.com>
2021-05-20 10:44:02 +0800

    优化

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 echo vm.max_map_count=655360 >> /etc/sysctl.conf
+if [ -d $PWD/relation/model ];then
+  echo "未找到模型，正在下载模型中...."
+  curl https://oss.jtyoui.com/data/wss-model.tar.gz
+  echo "解压模型中...."
+  tar -zxvf wss-model.tar.gz -C $PWD/relation
+fi
 sysctl -p
 docker-compose up -d
